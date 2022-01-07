48b312e54c532e527c9109b025271ddc617891b9
Abhishek Kumar Jha <72074598+AbhishekJha1916@users.noreply.github.com>
2022-01-07 23:00:55 +0530

    Update SpeechtoText.py

--- SpeechtoText.py
+++ SpeechtoText.py
@@ -4,4 +4,5 @@ with SR.Microphone() as source:
     r.adjust_for_ambient_noise(source)
     print("Listening...")
     audi=r.listen(source)
+print("Recognizing...")
 print("User Said : " + r.recognize_google(audi))
